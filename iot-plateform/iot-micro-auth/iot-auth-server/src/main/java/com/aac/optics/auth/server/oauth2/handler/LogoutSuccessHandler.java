ba93baa4fb177ddbcb72cdaba6a7dd5b35763490
Yan Shangqi <yanshangqi@aactechnologies.com>
2021-08-12 16:45:47 +0800

    2

--- iot-plateform/iot-micro-auth/iot-auth-server/src/main/java/com/aac/optics/auth/server/oauth2/handler/LogoutSuccessHandler.java
+++ iot-plateform/iot-micro-auth/iot-auth-server/src/main/java/com/aac/optics/auth/server/oauth2/handler/LogoutSuccessHandler.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Component
 public class LogoutSuccessHandler implements org.springframework.security.web.authentication.logout.LogoutSuccessHandler {
