b4a3c78d38ec4e59219ec62d158775183d22fe64
60054916 <yanshangqi@aactechnologies.com>
2021-12-18 11:09:33 +0800

    优化

--- iot-plateform/iot-provider-notification/src/main/java/com/aac/optics/provider/notification/service/impl/MailServiceImpl.java
+++ iot-plateform/iot-provider-notification/src/main/java/com/aac/optics/provider/notification/service/impl/MailServiceImpl.java
@@ -3,12 +3,14 @@ package com.aac.optics.provider.notification.service.impl;
 
 import com.aac.optics.provider.notification.entity.po.EmailSend;
 import com.aac.optics.provider.notification.service.MailService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.util.List;
@@ -16,7 +18,7 @@ import java.util.List;
 @Service
 public class MailServiceImpl implements MailService {
 
-    @Autowired
+    @Resource
     JavaMailSender mailSender;
 
     //@Value(value = "${spring.profiles}")
@@ -24,6 +26,8 @@ public class MailServiceImpl implements MailService {
 
     @Override
     public boolean send(EmailSend emailSend) throws MessagingException {
+        if(StringUtils.isBlank(emailSend.getEmailContent()))
+            return false;
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper;
         helper = new MimeMessageHelper(message, true);
