09d49b908a4d3516d9ca00c05e6001c924541b2f
60054916 <yanshangqi@aactechnologies.com>
2022-01-11 15:57:02 +0800

    优化

--- iot-plateform-vue/src/views/lenspacker/LensPackerMonitor.vue
+++ iot-plateform-vue/src/views/lenspacker/LensPackerMonitor.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <div class="container">
-      <mqtt-client ref="mqttClient" :value="lenspackerTopic" @messageArrived="messageArrived"/>
       <div style="margin-bottom: 20px">
         <div v-for="(item, index) of statusRadio" :key="index" class="status_radio_type"
              :style="'background-color:' + getStatusRadioColor(item) + ';text-align:center'"
@@ -9,7 +8,7 @@
           {{ item + '(' + this.statusCount[item] + ')' }}
         </div>
       </div>
-      <el-card v-for="(val, key, index) in LensPackerMachineInfo" :key="index"
+      <el-card v-for="(singleMachineInfo, index) of LensPackerMachineInfo" :key="index"
                :body-style="{ padding: '0px', height:'120px'}"
                shadow="hover"
                class="lenspacker_card_type"
@@ -22,7 +21,7 @@
                   style="text-align: center;height:30px; font-weight: bold;font-size: 16px;border-bottom: 1px solid cornflowerblue">
                 <el-col :span="24">
                   <p style="text-align: center;font-weight: bold;color: #008000;font-size: 24px">
-                    {{ key.substr(4) }}</p>
+                    {{ singleMachineInfo.machineNo.substr(4) }}</p>
                 </el-col>
                 <!--                <el-col :span="16">-->
                 <!--                    {{ getMachineStatus(singleMachineInfo.status) }}-->
@@ -35,27 +34,27 @@
                   <!--                  <div v-if="singleMachineInfo.status === 2" :style="'background-color:' + statusType[getMachineStatus(singleMachineInfo.status)] + ';height:30px;line-height:30px'">-->
                   <!--                    {{ getMachineStatus(singleMachineInfo.status) + ':' + singleMachineInfo.alarmInfo }}-->
                   <!--                  </div>-->
-                  <div v-if="val.Status === 2"
-                       :style="'background-color:' + statusType[getMachineStatus(val.Status)] + ';height:30px;line-height:30px'">
+                  <div v-if="singleMachineInfo.status === 2"
+                       :style="'background-color:' + statusType[getMachineStatus(singleMachineInfo.status)] + ';height:30px;line-height:30px'">
 
-                    {{ val.AlarmInfo }}
+                    {{ singleMachineInfo.alarmInfo }}
                   </div>
                   <div v-else
-                       :style="'background-color:' + statusType[getMachineStatus(val.Status)] + ';height:30px;line-height:30px'">
-                    {{ getMachineStatus(val.Status) }}
+                       :style="'background-color:' + statusType[getMachineStatus(singleMachineInfo.status)] + ';height:30px;line-height:30px'">
+                    {{ getMachineStatus(singleMachineInfo.status) }}
                   </div>
                 </el-col>
               </el-row>
               <el-row style="height: 30px;line-height: 30px">
-                <el-col :span="12">产能：{{ val.OutputQty }}</el-col>
-                <el-col :span="12">节拍：{{ val.MachineCT.toFixed(2) }}</el-col>
+                <el-col :span="12">产能：{{ singleMachineInfo.outputQty }}</el-col>
+                <el-col :span="12">节拍：{{ singleMachineInfo.machineCT.toFixed(2) }}</el-col>
               </el-row>
               <el-row style="height: 30px;line-height: 30px">
-                <el-col :span="12" v-if="val.status !== 0">
-                  类型：{{ val.CavityNums === 24 ? 24 : 16 }}穴
+                <el-col :span="12" v-if="singleMachineInfo.status !== 0">
+                  类型：{{ singleMachineInfo.cavityNums === 24 ? 24 : 16 }}穴
                 </el-col>
                 <el-col :span="12" v-else>类型：</el-col>
-                <el-col :span="12">握手信号：{{ val.IsComplete }}</el-col>
+                <el-col :span="12">握手信号：{{ singleMachineInfo.isComplete }}</el-col>
               </el-row>
             </div>
           </el-col>
@@ -67,34 +66,17 @@
 
 <script>
 import {getMachineStatus, getMachineAlarms} from "@/api/iot/lenspacker";
-import MqttClient from "@/components/MqttClient";
 
 export default {
   name: "CoatingMachineMonitor",
-  components: {
-    MqttClient
-  },
   created() {
-    //this.getLensPackerStatus()
+    this.getLensPackerStatus()
     this.timer = setInterval(() => {
-      //this.getLensPackerStatus()
+      this.getLensPackerStatus()
     }, 10000)
   },
   computed: {
-    lenspackerTopic() {
-      return [
-        {
-          topic: 'LensPacker/status/' + this.floorInfo + '/+',
-          qos: 0
-        }
-      ]
-    },
     LensPackerMachineInfo() {
-      return this.getLensPackerMachineInfo()
-    }
-  },
-  methods: {
-    getLensPackerMachineInfo(){
       const pages = []
       this.setDefaultCount()
       const position = this.$route.query.position;
@@ -102,43 +84,19 @@ export default {
         this.setStatusRadioValue(this.$route.params.status);
         this.refreshPage()
       }
-      for (const item in this.LensPackerInfoList) {
-        const statusName = this.getMachineStatus(this.LensPackerInfoList[item].Data.Status)
-        if (item.indexOf(position) === 0) {
+      this.LensPackerInfoList.forEach((item) => {
+        const statusName = this.getMachineStatus(item.status)
+        if (item.machineNo.indexOf(position) === 0) {
           if (this.statusRadioValue.indexOf(statusName) > -1) {
-            pages[item] = this.LensPackerInfoList[item].Data
+            pages.push(item)
           }
           this.statusCount[statusName]++
         }
-      }
-      return this.sortObjByKey(pages)
-    },
-    sortObjByKey(obj) {
-      const keys = Object.keys(obj).sort();
-      const newObj = {};
-      for (let i = 0; i < keys.length; i++) {
-        const index = keys[i];
-        newObj[index] = obj[index];
-      }
-      return newObj;
-    },
-    initConnect() {
-      this.$nextTick(() => {
-        this.$refs.mqttClient.createMqttConnection();
-      });
-    },
-    //接收消息
-    messageArrived(msg) {
-      switch (msg.Message) {
-        case 'status':
-          this.LensPackerInfoList[msg.ClientId] = msg
-          break;
-      }
-    },
-    //断开连接
-    disconnect() {
-      this.$refs.mqttClient.destroyMqttConnection();
-    },
+      })
+      return pages
+    }
+  },
+  methods: {
     refreshPage() {
       this.$router.push(this.$route.fullPath)
     },
@@ -193,26 +151,12 @@ export default {
       })
     }
   },
-  mounted() {
-    let _this = this;
-    setTimeout(function() {
-      const position = _this.$route.query.position
-      _this.floorInfo = position.substring(2, position.length)
-      _this.siteInfo = _this.$route.query.site
-      _this.initConnect()
-    }, 100);
-  },
   beforeUnmount() {
     clearInterval(this.timer);
   },
   data() {
     return {
-      floorInfo: '',
-      siteInfo: '',
-      client: {
-        connected: false,
-      },
-      LensPackerInfoList: {},
+      LensPackerInfoList: [],
       // statusType: {
       //   '设备离线': 'item_coating_offline',
       //   '正常运行': 'item_coating_running',
@@ -231,20 +175,6 @@ export default {
         '正常运行': 0,
         '报警': 0
       }
-    }
-  }
-  ,
-  watch: {
-    $route: {
-      handler() {
-        let _this = this;
-        setTimeout(function () {
-          _this.floorInfo = _this.$route.query.position.substring(2, _this.$route.query.position.length)
-          _this.siteInfo = _this.$route.query.site
-          _this.initConnect()
-        }, 100);
-      },
-      deep: true,
     }
   }
 };
