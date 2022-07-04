865768ecc1584397f9eee85c557ba49288345c74
60054916 <yanshangqi@aactechnologies.com>
2022-07-04 14:26:07 +0800

    修改MQTT连接

--- iot-plateform-vue/src/utils/mqttConstant.js
+++ iot-plateform-vue/src/utils/mqttConstant.js
@@ -1,9 +1,11 @@
 export default {
     MQTT_CONNECTION: {
-        host: '10.69.76.40',
-        port: 8083,
+        host: 'k8s.aacoptics.com',
+        port: 30931,
         endpoint: '/mqtt',
         clean: true, // 保留会话
+        username: "admin",
+        password: "123456!a",
         connectTimeout: 5000, // 超时时间
         reconnectPeriod: 5000, // 重连时间间隔
     },
